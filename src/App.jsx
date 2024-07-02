63176f345b95980dd58381149f16253171be2a53
Maimuna-shaon <shawon213236@gmail.com>
2024-07-03 00:11:21 +0600

    Updated class

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,114 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import BioData from './component/BioData';
+
 
 function App() {
   /*const name='shawon';
   console.log(name,"name");
   const skills=["js","React","Redux"];*/
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([
+    {id: "1", name: "Mahir"},
+    {id: "2" , name:"Nahid"},
+  ]);
+  
+  const [editMode,setEditMode] = useState(false);
+  
+  const [editableStudent, setEditableStudent] = useState(null);
+  
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if(studentName.trim() === "")
+      return alert('Please provide a valid name');
+    editMode ? updateHandler() : createHandler();
+  };
 
-  return (
-    <div className="App">
-      <BioData
-      name="Mahir Asif"
-      age={27}
-      email="asifmahir@gmail.com"
-      skills={["React","Redus","NodeJS"]}
-      interests={["System Design","Football","Chess"]}
-      sociallinks={[
-        {medianame:"fb",
-        mediaAdd:"FB.com/asifmahir"},
-        {medianame:"Github",
-        mediaAdd:"github.com/asifmahir"},
-        {medianame:"LinkedIn",
-        mediaAdd:"LinkedIn.com/asifmahir"},
-      ]}
-      />
-     <hr />
-     
+  const createHandler = () => {
+    const newStudent = {
+      id: Date.now() + "",
+      name: studentName
+    }
+    setStudents([...students,newStudent]);
+    setStudentName("");
+  };
+
+  const editHandler = (student) => {
+    setEditMode(true);
+    setStudentName(student.name);
+    setEditableStudent(student);
+  };
+
+  const updateHandler = () => {
+    /*const targetedIndex= students.findIndex(
+      (student) => student.id === editableStudent.id,
+
+    );*/
+    const updatedStudentList =students.map((student)=>{
+      if(student.id=== editableStudent.id){
+        return {...student, name: studentName};
+        //student.name = studentName;
+      }
+      return student;
+    });
+    setStudents(updatedStudentList);
+    setStudentName('');
+    setEditMode(false);
+    setEditableStudent(null);
+  };
+
+  const removeHandler = (studentId) => {
+    const updatedStudentList = students.filter(
+     (student)=> student.id!=studentId,
+    );
+    setStudents(updatedStudentList);
+  };
+  
+
+return (
+ <div className="App">
+  <form onSubmit={submitHandler} className="form">
+    <input type="text" value={studentName} 
+    onChange ={(e)=> setStudentName(e.target.value)} />
+  <button type="submit">
+    {editMode ? "Update Student" : "Create Student"}
+  </button>
+  </form>
+  <div className="student-section">
+    <div className="list all-list">
+      <h2>All students</h2>
+      <ul>
+        {students.map(student=>(
+          <li className="list-item" key={student.id}> 
+          <span>{student.name}</span>
+          <button onClick ={() => editHandler(student)}>
+            Edit</button>
+          <button onClick = { () => removeHandler(student.id)}>
+            Remove
+          </button>
+          <button>
+            Sent to Present List
+          </button>
+          <button>
+            Sent to Absent List
+          </button>
+          </li>
+        ))}
+      </ul>
     </div>
+    <div className="list present-list">
+    <h2>Present students</h2>
+    </div>
+     <div className="list absent-list">
+     <h2>Absent students</h2>
+     </div>
 
-  );
+
+  </div>
+  
+</div>
+);
+  
 }
 
 export default App
--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,5 @@
-const add=(a,b)=>{
- return a+b;
+const add = (a, b) => {
+	return a + b;
 };
+
 export default add;
